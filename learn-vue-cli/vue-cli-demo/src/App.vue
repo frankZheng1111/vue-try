e3707e1fb3cbd7da9d4641cd45d7a5292cc4b82f
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-01 16:11:49 +0800

    update index page

--- learn-vue-cli/vue-cli-demo/src/App.vue
+++ learn-vue-cli/vue-cli-demo/src/App.vue
@@ -17,5 +17,5 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "./style/scss/style";
+  @import "./style/style";
 </style>
--- learn-vue-cli/vue-cli-demo/src/page/index.vue
+++ learn-vue-cli/vue-cli-demo/src/page/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>index</div>
+  <div>
+    <h1 class='logo'>Cnodejs Api Test</h1>
+    <ul class='list'>
+      <li v-for="item in lists" v-text="item.title"></li>
+    </ul>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          id: 1,
+          title: "test title1"
+        },
+        {
+          id: 2,
+          title: "test title2"
+        }
+      ]
+    }
+  }
+}
+</script>
