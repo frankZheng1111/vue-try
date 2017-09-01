83d33dc50ebf824df27ed962a8ecc6bbc439ddfb
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-01 14:44:29 +0800

    update vue router and add 2 page

--- learn-vue-cli/vue-cli-demo/src/router/index.js
+++ learn-vue-cli/vue-cli-demo/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+// import Hello from '@/components/Hello'
+
+import index from '../page/index.vue'
+import content from '../page/content.vue'
 
 Vue.use(Router)
 
@@ -8,8 +12,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: index
+    },
+    {
+      path: '/content',
+      // name属性可以在后续使用这条路径规则的时候，直接引用。
+      name: 'content',
+      component: content
     }
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // }
   ]
 })
