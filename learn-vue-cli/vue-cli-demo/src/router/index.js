2be49db453ffcbebdddf93bc3a1fe925ebb2cb90
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-01 15:20:29 +0800

    Add scss

--- learn-vue-cli/vue-cli-demo/src/router/index.js
+++ learn-vue-cli/vue-cli-demo/src/router/index.js
@@ -15,8 +15,8 @@ export default new Router({
       component: index
     },
     {
-      path: '/content',
       // name属性可以在后续使用这条路径规则的时候，直接引用。
+      path: '/content',
       name: 'content',
       component: content
     }
