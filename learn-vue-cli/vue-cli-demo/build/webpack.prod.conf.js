75d8d952d37dc08f171062341a8d6bbfa3a83066
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-02 15:58:47 +0800

    Add api url config

--- learn-vue-cli/vue-cli-demo/build/webpack.prod.conf.js
+++ learn-vue-cli/vue-cli-demo/build/webpack.prod.conf.js
@@ -29,7 +29,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      'process.env': env,
+      'API_URL': '\'https://cnodejs.org/api/v1\''
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
