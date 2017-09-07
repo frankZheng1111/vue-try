a7bb2a9fdbde5a35c4cc1053e701210d74b3416b
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-07 18:12:21 +0800

    update router

---
+++ src/components/home/topicsTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <mt-navbar v-model="selected">
+    <mt-tab-item v-for="tab in tabs" :id="tab" v-text="tabTexts[tab]" @click.native="tabSelected">
+    </mt-tab-item>
+  </mt-navbar>
+
+</template>
+<script>
+import Vue from 'vue'
+import { Navbar, TabItem } from 'mint-ui';
+
+Vue.component(Navbar.name, Navbar);
+Vue.component(TabItem.name, TabItem);
+export default {
+  data() {
+    return {
+      selected: '',
+      tabs: ['', 'ask', 'share', 'job', 'good', 'dev'],
+      tabTexts: {
+        '': '全部',
+        good: '精华',
+        share: '分享',
+        ask: '问答',
+        job: '招聘',
+        dev: '客户端测试'
+      }
+    }
+  },
+
+  methods: {
+    tabSelected() {
+      this.$emit('changeTab', this.selected)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../../style/components/home/topicsTabBar";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -15,11 +15,16 @@ export default new Router({
       component: home
     },
     {
+      path: '/',
+      redirect: '/home'
+    },
+    {
       // name属性可以在后续使用这条路径规则的时候，直接引用。
       //
       path: '/content',
       name: 'content',
       component: content
-    }
+    },
+    { path: '*', redirect: '/home' }
   ]
 })
