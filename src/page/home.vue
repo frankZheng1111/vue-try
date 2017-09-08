bd8b3d51882a83c2e92061d68e727f67cc9f8f48
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-08 14:07:33 +0800

    Add spinner for placeholder

--- src/page/home.vue
+++ src/page/home.vue
@@ -3,13 +3,17 @@
     <h1 class='logo'>Cnodejs Api Test</h1>
     <topics-tab-bar @changeTab="changeTab"></topics-tab-bar>
     <topics-list :topics="topics" :loading= "loading" @loadMore="loadMore"></topics-list>
-    <div class='loading-placeholder'><p>加载中...</p></div>
+    <mt-spinner class='loading-placeholder' type="fading-circle" color="#26a2ff" :size="50"></mt-spinner>
   </div>
 </template>
 <script>
+import Vue from 'vue'
 import topicsList from '../components/home/topicsList'
 import topicsTabBar from '../components/home/topicsTabBar'
 import * as api from '../api'
+
+import { Spinner } from 'mint-ui'
+Vue.component(Spinner.name, Spinner)
 
 
 export default {
