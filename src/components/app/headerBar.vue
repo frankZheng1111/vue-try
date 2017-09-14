f7520e77fb1fae623adb48fc64cbaf2f948fd8a4
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-14 17:44:53 +0800

    update sidebar and headerbar

--- src/components/app/headerBar.vue
+++ src/components/app/headerBar.vue
@@ -1,43 +1,38 @@
 <template>
   <div>
-  <mt-header :title="tabTexts[selected]" class="topic-header">
-    <span slot="left" @click="toggleSidebar">菜单</span>
-  </mt-header>
-  <sidebar :activeSidebar="activeSidebar" @hideSidebar="toggleSidebar"></sidebar>
-  <mt-navbar v-model="selected">
-    <mt-tab-item v-for="tab in tabs" :id="tab" :key="tab">
-      <router-link :to="{ name: 'topics', query: { tab: tab || undefined } }">
-        {{ tabTexts[tab] }}
-      </router-link>
-    </mt-tab-item>
-  </mt-navbar>
+    <mt-header :title="headerBarTitle" class="topic-header">
+      <span slot="left" @click="toggleSidebar">菜单</span>
+    </mt-header>
+    <sidebar :activeSidebar="activeSidebar" @hideSidebar="toggleSidebar"></sidebar>
   </div>
 </template>
 <script>
 import Vue from 'vue'
-import { Header, Navbar, TabItem } from 'mint-ui'
+import { Header } from 'mint-ui'
 import TAB_TEXTS from '../../config/tabTexts'
 
 import Sidebar from './sidebar'
 
 Vue.component(Header.name, Header)
-Vue.component(Navbar.name, Navbar)
-Vue.component(TabItem.name, TabItem)
 
 export default {
   data() {
     return {
-      selected: this.selectedTab || '',
-      tabs: ['', 'ask', 'share', 'job', 'good', 'dev'],
-      tabTexts: TAB_TEXTS,
       activeSidebar: false
     }
   },
   props: ['selectedTab'],
 
+  computed: {
+    headerBarTitle() {
+      return TAB_TEXTS[this.$route.query.tab]
+    }
+  },
+
   components: {
     'sidebar': Sidebar
   },
+
 
   methods: {
     toggleSidebar() {
