2c55a5da130c233d4e74fed9fdf25f92a76782da
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-16 17:07:01 +0800

    Add main topic component

---
+++ src/components/topic/main-topic.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="topic" class="main-topic">
+    <section class="topic-info">
+      <img class="avatar avatar-small pull-left" :src="topic.author.avatar_url" :title="topic.author.loginname"/>
+      <div class="topic-detail">
+        <p>
+        <span class="author-name" >{{ topic.author.loginname }}</span>
+        <mt-badge class="topic-tab" size="large" color="#888">{{ '置顶' }}</mt-badge>
+        </p>
+        <p>
+          <time>{{ '发布于xx前' }}</time>
+          <span>xxxxx次浏览</span>
+        </p>
+      </div>
+    </section>
+    <div class="topic-content" v-html="topic.content"></div>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import { Badge } from 'mint-ui'
+
+Vue.component(Badge.name, Badge);
+
+export default {
+  data() {
+    setTimeout(() => { console.log(this.topic); }, 10000)
+    return {
+    }
+  },
+
+  props: ['topic'],
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../../style/components/topic/main-topic";
+</style>
--- src/views/topic.vue
+++ src/views/topic.vue
@@ -1,28 +1,12 @@
 <template>
   <div v-if="topic" class="topic-page">
     <h2 class="topic-title">{{ topic.title }}</h2>
-    <section class="topic-info">
-      <img class="avatar avatar-small pull-left" :src="topic.author.avatar_url" :title="topic.author.loginname"/>
-      <div class="topic-detail">
-        <p>
-        <span class="author-name" >{{ topic.author.loginname }}</span>
-        <mt-badge class="topic-tab" size="large" color="#888">{{ '置顶' }}</mt-badge>
-        </p>
-        <p>
-          <time>{{ '发布于xx前' }}</time>
-          <span>xxxxx次浏览</span>
-        </p>
-      </div>
-    </section>
-    <div class="topic-content" v-html="topic.content"></div>
+    <main-topic :topic="topic"></main-topic>
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import MainTopic from '../components/topic/main-topic'
 import * as api from '../api'
-import { Badge } from 'mint-ui'
-
-Vue.component(Badge.name, Badge);
 
 export default {
   data() {
@@ -33,6 +17,7 @@ export default {
   },
 
   components: {
+    'main-topic': MainTopic
   },
 
   created() {
