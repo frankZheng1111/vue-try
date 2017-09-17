e8599c8f10df6789aa51fbd3504c9f4170d2050e
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-17 21:01:14 +0800

    Add topicComments component

---
+++ src/components/topic/commentList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="topic" class="topic-comments">
+  </div>
+</template>
+
+<script>
+'use strict'
+
+export default {
+}
+</script>
+
+<style lang="scss">
+  @import "../../style/components/topic/topicComments";
+</style>
