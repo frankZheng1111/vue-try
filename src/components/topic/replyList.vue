69b7732adf38d2428126255ba31a5b74681df460
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-18 16:06:07 +0800

    update replylist style

--- src/components/topic/replyList.vue
+++ src/components/topic/replyList.vue
@@ -2,7 +2,23 @@
   <div v-if="replies && replies.length" class="topic-replies">
     <ul class="reply-list">
       <li v-for="reply in replies" :key="reply.id" :id="reply.id" class="reply">
-        <div class="markdown-body" v-html="reply.content">
+        <div class="replyer-info">
+          <img class="avatar avatar-small" :src="reply.author.avatar_url" :title="reply.author.loginname"/>
+          <div class="replyer-detail-info">
+            <p>
+              <span class="replyer.loginname">{{ reply.author.loginname }}</span>
+              <span>{{ `#${replies.indexOf(reply) + 1}` }}</span>
+            </p>
+            <p>
+              <time>{{ reply.create_at }}</time>
+              <span class="reply-action">
+                <span>赞(1)</span>
+                <span>回复</span>
+              </span>
+            </p>
+          </div>
+        </div>
+        <div class="reply-content markdown-body" v-html="reply.content">
         </div>
       </li>
     </ul>
