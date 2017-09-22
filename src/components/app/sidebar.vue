f9d8270a37c468f4b4c83013317899cf5162e639
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-09-22 10:15:28 +0800

    Update sidebar

--- src/components/app/sidebar.vue
+++ src/components/app/sidebar.vue
@@ -4,6 +4,11 @@
       <div v-if="activeSidebar" class="page-cover" @click="hideSidebar"></div>
     </transition>
     <section :class="{ sidebar: true, show: activeSidebar }">
+      <section v-if="isUserLogin()" class="user-info">
+      </section>
+      <section v-else class="login">
+        <button class="login-btn" @click="showLoginOptions">登录</button>
+      </section>
       <section class="tab-list">
         <router-link v-for="tab in tabs" :id="tab" :key="tab" :to="{ name: 'topics', query: { tab: tab || undefined } }" @click.native="hideSidebar">
           {{ tabTexts[tab] }}
@@ -13,18 +18,29 @@
   </div>
 </template>
 <script>
+'use strict'
+
 import TAB_TEXTS from '../../config/tabTexts'
 
 export default {
+  props: ['activeSidebar'],
+
   data() {
     return {
       tabs: ['', 'ask', 'share', 'job', 'good', 'dev'],
       tabTexts: TAB_TEXTS
     }
   },
-  props: ['activeSidebar'],
 
   methods: {
+    isUserLogin() {
+      return false
+    },
+
+    showLoginOptions() {
+      alert('loginOptions')
+    },
+
     hideSidebar() {
       this.$emit('hideSidebar')
     }
