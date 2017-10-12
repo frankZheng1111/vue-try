2c7f6fc7d4e39b4cddac6302d069f1420010f8c5
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-10-12 15:55:29 +0800

    Add getuserprofile

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,25 +2,45 @@
   <div class="user-page">
     <div class="user-info">
     </div>
-    <div class="user-actions">
-    </div>
+    <topic-list :topics="userProfile.recentTopics" :loading="false"></topic-list>
+    <topic-list :topics="userProfile.recentReplies" :loading="false"></topic-list>
   </div>
 </template>
 
 <script>
 'use strict'
 
+import TopicList from '../components/user/TopicList'
+import * as api from '../api'
+
 export default {
   name: 'About',
   data() {
     return {
+      loginname: this.$route.params.loginname,
+      userProfile: {}
     }
   },
 
+  components: {
+    'topic-list': TopicList,
+    'reply-list': ReplyList,
+  },
+
+  created() {
+    this.getUserProfile()
+  },
+
   methods: {
+    async getUserProfile() {
+      let { data: { data: userProfile } } = await api.getUserProfile(this.loginname)
+      this.userProfile = userProfile
+      return
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
 </style>
