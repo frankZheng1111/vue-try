2600d0f77dc2e7446ad74f52cde15a7bfd83eeee
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-10-22 21:32:56 +0800

    Add Userentrance component

---
+++ src/components/app/UserEntrance.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="{ name: 'user', params: { loginname: loginName } }">
+    <slot></slot>
+  </router-link>
+</template>
+<script>
+'use strict'
+
+export default {
+  name: 'UserEntrance',
+  props: {
+    loginName: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
